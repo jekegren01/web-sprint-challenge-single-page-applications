b730f82bb91e5000fc9c494cd59edddcb987ce3d
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-07-19 16:45:49 -0700

    Added header, pic of pizza, 2 links for home and help.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Header from './components/Header';
+import Pizza from './Assets/Pizza.jpg';
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <img src={Pizza} alt='pic of pizza' />
+      <Header />
+    </div>
   );
 };
 export default App;
